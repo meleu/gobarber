4a702b3f2883f0e1fdb474e7902397608ad3af31
meleu <meleuzord@yahoo.com>
2020-10-23 21:18:52 -0300

    AppointmentsRepository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,31 +1,29 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => response.json(appointments));
+appointmentsRouter.get('/', (request, response) =>
+  response.json(appointmentsRepository.all())
+);
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentByDate = appointments.find((appointment) =>
-    isEqual(parsedDate, appointment.date)
-  );
+  const parsedDate = parseISO(date);
+  const appointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
 
-  if (findAppointmentByDate) {
+  if (appointmentInSameDate) {
     return response
       .status(400)
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
