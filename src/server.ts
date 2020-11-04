fc1c76b7b16fdab909657320ff48c17ae7cc10c5
meleu <meleuzord@yahoo.com>
2020-11-04 17:25:50 -0300

    serve static files

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import express from 'express';
 import './database';
 
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => console.log('🚀 backend has been launched!'));
