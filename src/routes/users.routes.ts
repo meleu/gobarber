c8637d03b0f59b26888de8b1e52395651cb169c8
meleu <meleuzord@yahoo.com>
2020-11-04 13:11:42 -0300

    handling uploads

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const uploadMiddleware = multer(uploadConfig);
 
 const usersRouter = Router();
 
@@ -29,5 +34,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  uploadMiddleware.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  }
+);
 
 export default usersRouter;
